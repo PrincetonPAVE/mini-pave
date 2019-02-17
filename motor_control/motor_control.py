dd14c3c31b2b1abba60d9f0bb028513ddb534463
Haicheng Charles Zhao <czhao39@gmail.com>
2019-02-17 14:50:44 -0500

    Add Pi code for motor

---
+++ motor_control/motor_control.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+
+MOTOR_CONTROL = 12
+
+GPIO.setup(MOTOR_CONTROL, GPIO.OUT, initial=GPIO.LOW)
+
+try:
+    while True:
+        GPIO.output(MOTOR_CONTROL, GPIO.HIGH)
+        time.sleep(1)
+finally:
+    GPIO.cleanup()
